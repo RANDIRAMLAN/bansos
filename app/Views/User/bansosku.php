429fd44588b1246371fb7c4fdf086df2c5b25e1f
RANDIRAMLAN <rajahati30h@gmail.com>
2020-11-05 13:48:08 +0800

    ubah border kartu bansos

--- app/Views/User/bansosku.php
+++ app/Views/User/bansosku.php
@@ -4,9 +4,9 @@
     <div class="row">
         <?php foreach ($bansosku as $b) { ?>
             <div class="col col-md-4 mt-2">
-                <div class="card border-dark">
-                    <div class="card-header text-center"><?= $b['namaBansos']; ?></div>
-                    <div class="card-body text-center">
+                <div class="card border-info">
+                    <div class="card-header text-center bg-info text-white"><?= $b['namaBansos']; ?></div>
+                    <div class="card-body text-center bg-light">
                         <h5 class="card-title"><?= $b['noKK']; ?> / <?= $b['kepalaKeluarga']; ?></h5>
                     </div>
                 </div>
